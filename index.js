07726a16ea78ae87a2fdbdb9f55ade6147f9ed3d
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-04-28 11:21:44 -0400

    commit tree db500feb0cc26537c9ffb23a9c3472d92dd5b6e5

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 29
+const num4 = 5
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = num3 % num4 
+const max = Math.max(1,3,5,7,9,13,16,17,20)
